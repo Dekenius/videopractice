505f1f290301ec3d351cbb8b29a7269664d67413
Dekenius <dankrasno@gmail.com>
2020-04-28 18:31:09 -0400

    can find dominant color

--- videos.py
+++ videos.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import cv2
-
+import math
 import imutils      # allows video editing
 from imutils.object_detection import non_max_suppression
 
@@ -10,15 +10,14 @@ cap = cv2.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+# out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
-
-#default HOG
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-
+find_faces = False
+avg_color = True
 i = 0
+
+
+
 while(cap.isOpened()):
     i += 1
 
@@ -36,37 +35,63 @@ while(cap.isOpened()):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.equalizeHist(gray)
 
-        # color = (57,255,20)
 
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-        eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        if find_faces:
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 
-        #     cv2.rectangle(img, (xA, yA), (xB, yB), color, 2)
+            # change the second param (scaleFactor)
+            faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+            for (x,y,w,h) in faces:
+                frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                # I only have 2 eyes, keeping this until 3+ eyes in video
+                eyes = eyes[:2]
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-        # # draw bounding boxes
-        # for (x, y, w, h) in rects:
-        #     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-        #
-        # cv2.rectangle(frame, (5,5), (100, 100), color, 5)
+        if avg_color:
+
+            average = frame.mean(axis=0).mean(axis=0)
+            # print(average)
+            # print("hi")
+
+            pixels = np.float32(frame.reshape(-1, 3))
+
+            n_colors = 5
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+            flags = cv2.KMEANS_RANDOM_CENTERS
+
+            _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+            _, counts = np.unique(labels, return_counts=True)
+
+            dominant = palette[np.argmax(counts)]
+
+            x,y,w,h = 5,5,100,100
+
+            print(dominant)
+
+            t2 = tuple(map(lambda x: math.ceil(x), dominant))
+
+            print(t2)
+
+            # checked that it works using this, try placing oddly colored objects in front of your camera
+            # frame = cv2.rectangle(frame,(x,y),(x+w,y+h), t2,10)
+
+            # print(dominant)
+
         cv2.imshow('frame',frame)
-        #
-        # if i == 10:
-        #     print(frame.shape)
-        #     print(rects)
-        #
-        # if len(rects) != 0:
-        #     print("see you")
+        #debugging
+        if i == 3:
+            # print(frame.shape)
+            # print(len(frame[0]))
+            # print(frame)
+            # break
+            pass
+
 
         # cv2.
         if cv2.waitKey(1) & 0xFF == ord('q'):
